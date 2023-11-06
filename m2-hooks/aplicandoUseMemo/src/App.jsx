63756918aca912c0b37a31ed1f9d4d7db19d124d
abrillgomez <abrilgomezp@outlook.com>
2023-11-06 15:56:57 -0300

    useMemo

---
+++ m2-hooks/aplicandoUseMemo/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Statistics } from './hooks/useMemo/Statistics'
+
+const App = () => {
+
+  return (
+    <>
+    <Statistics />
+    </>
+  )
+}
+
+export default App
---
+++ m2-hooks/aplicandoUseMemo/src/hooks/useMemo/Statistics.jsx
@@ -0,0 +1,27 @@
+import { useState, useMemo } from "react";
+import { data } from "../../database";
+
+const average = (list) => {
+  console.log("Calculando Average");
+  const suma = list.reduce((a, b) => a + b.amount, 0);
+  return `El promedio es ${suma / list.length}`;
+};
+
+export const Statistics = () => {
+  const [show, setShow] = useState(false);
+
+  const averageMemo = useMemo(() => average(data), []);
+  
+  console.log("Renderizado de Statistics");
+
+  const handleShow = () => {
+    setShow(!show);
+  };
+
+  return (
+    <div>
+      <h2>Statistics {averageMemo}</h2>
+      <button onClick={handleShow}>{show ? "Hide" : "Show"}</button>
+    </div>
+  );
+};
