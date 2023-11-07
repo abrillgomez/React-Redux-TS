6a8a0aae61d6ea1117dd1a2b3c88c0aeacfd73ad
abrillgomez <abrilgomezp@outlook.com>
2023-11-07 16:53:41 -0300

    jsdoc

---
+++ m2-hooks/jsdoc/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { ParentComponent } from './hooks/useCallback/ParentComponent'
+
+const App = () => {
+
+  return (
+    <>
+    <ParentComponent />
+    </>
+  )
+}
+
+export default App
---
+++ m2-hooks/jsdoc/src/index.js
@@ -0,0 +1,59 @@
+/**
+ * Nombre de usuario
+ * @type {string}
+ */
+let userName = "Abril"
+
+/**
+ * Edad del usuario
+ * @type {number}
+ */
+let userAge = 21
+
+
+/**
+ * Lista de edades de usuario
+ * @type {Array<Number>}
+ */
+const usersAge = [23, 24, 30, 45, 51]
+
+/**
+ * Lista de valores
+ * @type {Array<Number | String | Boolean>}
+ */
+const mixedArray = [42, 'Hola', true]
+
+/**
+ * Usuario
+ * @type {{id: number, name: string, age: number, isActive: boolean}}
+ */
+const user = {
+  id: 1,
+  name: "Abril",
+  age: 21,
+  isActive: true
+}
+
+/**
+ * @typedef {object} User
+ * @property {number} id este es el id del usuario
+ * @property {string} name este es el nombre del usuario
+ * @property {number} age esta es la edad del usuario
+ * @property {boolean} isActive si el usuario está activo
+ */
+const user2 = {
+  id: 1,
+  name: "Abril",
+  age: 21,
+  isActive: true
+}
+
+/**
+ * @type {User}
+ */
+const user3 = {
+  id: 1,
+  name: "Abril",
+  age: 21,
+  isActive: true,
+}
