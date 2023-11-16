eff33d4be8f4fd228bb57b214e42d9c189fbd6bd
abrillgomez <abrilgomezp@outlook.com>
2023-11-16 17:27:23 -0300

    ecommerce

---
+++ m7-ecommerce/src/main.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { LayoutMain } from "./components/Layouts/LayoutMain";
+import { Home } from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import { CartProvider } from "./context/CartProvider";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <LayoutMain />,
+		children: [
+			{ index: true, element: <Home /> },
+			{ path: "/cart", element: <Cart />}
+		]
+	}
+])
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<CartProvider>
+			<RouterProvider router={router} />
+		</CartProvider>
+	</React.StrictMode>
+);
---
+++ m7-ecommerce/src/pages/Home/Home.tsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react"
+import { Hero } from "../../components/ui/Hero"
+import styles from './Home.module.css'
+import { CardProduct } from "../../components/ui/CardProduct/CardProduct"
+
+export const Home = () => {
+
+  const [products, setProducts] = useState([])
+
+  const getProducts = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/products')
+      const data = await response.json()
+      setProducts(data)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+  
+  console.log(products);
+  
+
+  return (
+    <>
+      <Hero />
+      <div className={styles.container}>
+        {products.map((product) => (
+          <CardProduct key={product.tail} product={product} />
+        ))}
+      </div>
+    </>
+  )
+}
